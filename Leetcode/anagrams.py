ffc2286d2947d63383608020a40964adb00781cf
satyam-thakur <satyam.th3@gmail.com>
2024-11-27 19:24:41 -0500

    updates

--- Leetcode/anagrams.py
+++ Leetcode/anagrams.py
@@ -7,10 +7,11 @@ def isanagram(strs: list[str]) -> list[str]:
       sorted_s = tuple(sorted(s))
       anagrams_s[sorted_s].append(s)
     
-    for values in anagrams_s.values():
+    for values in anagrams_s.values-():
       result.append(values)
     return result
 
 print(isanagram(['cat','tac','hell','heck']))
-print(isanagram(['eat','tea','tan','ate','nat','bat']))
+print(isanagram(['eat','tea','tan','ate','nat','bat'])) 
+print(isanagram(['']))
 
