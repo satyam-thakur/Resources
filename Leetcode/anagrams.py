4ebc3fb5cd72f23db8015c2a66f2fe3ca262ab03
satyam-thakur <satyam.th3@gmail.com>
2025-03-27 19:39:41 -0400

    updated ssh config

--- Leetcode/anagrams.py
+++ Leetcode/anagrams.py
@@ -1,16 +1,28 @@
 from collections import defaultdict
+# def isanagram(strs: list[str]) -> list[str]:
+
+#     anagrams_s = defaultdict(list)
+#     result = []
+    
+#     for s in strs:
+#       sorted_s = tuple(sorted(s))
+#       print (sorted_s)
+#       anagrams_s[sorted_s].append(s)
+    
+#     for values in anagrams_s.values():
+#       result.append(values)
+#     return result
+
 def isanagram(strs: list[str]) -> list[str]:
     anagrams_s = defaultdict(list)
-    result = []
-    
     for s in strs:
-      sorted_s = tuple(sorted(s))
-      anagrams_s[sorted_s].append(s)
-    
-    for values in anagrams_s.values():
-      result.append(values)
-    return result
+        sorted_s = "".join(sorted(s))   # sorted(s) returns ['a', 'b', 'c'], while "".join(sorted(s)) returns "abc"
+        print (sorted_s)
+        anagrams_s[sorted_s].append(s)
+    return list(anagrams_s.values())
 
 print(isanagram(['cat','tac','hell','heck']))
 
+
+# from collections import defaultdict
 
--- Leetcode/balloon_count.py
+++ Leetcode/balloon_count.py
@@ -2,8 +2,9 @@ class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         # c = collections.Counter(text)
         c = {}
+        text =text.lower()
         for t in text:
             c[t] = 1 + c.get(t,0)
         return min(c['b'],c['a'],c['l']//2,c['o']//2,c['n'])
     
-print(Solution().maxNumberOfBalloons("nlballoonaebolko"))
+print(Solution().maxNumberOfBalloons("Nlballoonaebolko"))
